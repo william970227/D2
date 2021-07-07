1afef73d91888968425e6b650f02c11f03594656
william970227 <86942078+william970227@users.noreply.github.com>
2021-07-07 09:07:13 +0800

    Add files via upload

---
+++ D2.py
@@ -0,0 +1,46 @@
+
+X = input ("成績")
+X = int (X)
+if X>=60:
+    print("及格了")
+else:
+    print("不及格")
+                    
+Y = input("分數")
+Y = int (Y)
+if 59>=Y>=0:
+    print("E")
+if 79>=Y>=70:
+    print("C")
+if 69>=Y>=60:
+    print ("D")
+if 89>=Y>=80:
+    print("B")
+if 100>=Y>=90:
+    print ("A")
+    
+A = input ("數學成績")
+B = input ("英文成績")
+A = int (A)
+B = int (B)
+
+if A>=90 and B>=90:
+    print("有獎學金")
+elif A==100 or B==100:
+    print("有獎學金")
+else:
+    print("無法獲得獎學金")
+
+if A >= 90 and B >=90:
+    print("有獎勵")
+elif A<60 and B<60:
+    print("有懲罰")
+elif A<60 or B<60:
+    print("再加油")
+    
+    
+    
+    
+           
+       
+ 
